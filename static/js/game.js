fc7c94c1f82f4d84766e07a8ce8675bc6814408f
Drosztmér István <drosztmer.istvan@gmail.com>
2019-09-10 14:34:48 +0200

    Add get random file function

--- static/js/game.js
+++ static/js/game.js
@@ -2,3 +2,10 @@ document.getElementById('restart').addEventListener('click', retry);
 function retry() {
     window.location.reload();
 }
+
+function getRandomLink() {
+    let images = document.getElementById("file-container").dataset.files;
+    images = JSON.parse(images);
+    let randomLink = images[Math.floor(Math.random() * images.length)];
+    return randomLink
+}
