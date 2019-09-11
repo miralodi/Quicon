edeec812215a51c003d49e5a4a44f0885665b8cf
Drosztmér István <drosztmer.istvan@gmail.com>
2019-09-11 14:16:22 +0200

    Add compare cards and set scores

--- static/js/game.js
+++ static/js/game.js
@@ -1,5 +1,8 @@
 function main() {
 
+    let score1 = 0;
+    let score2 = 0;
+
     try {
         document.getElementById('restart').addEventListener('click', retry);
     } catch (error) {
@@ -15,66 +18,90 @@ function main() {
 
     }
 
-    getCards();
-    document.addEventListener('keydown', inputKeyUp);
 
-}
+    try {
+        getCards();
+        document.addEventListener('keydown', compareCards);
+    } catch (error) {
 
-
-function retry() {
-    window.location.reload();
-}
+    }
 
 
-function getRandomLink(images) {
-    return images[Math.floor(Math.random() * images.length)];
-}
+    function retry() {
+        window.location.reload();
+    }
 
 
-function highlight(event) {
-    event.target.select();
-}
+    function getRandomLink(images) {
+        return images[Math.floor(Math.random() * images.length)];
+    }
 
 
-function getPlayerCards(playerCards, middleCard) {
-    let randomIndex = Math.floor(Math.random() * playerCards.length);
-    let images = document.getElementById("file-container").dataset.files;
-    images = JSON.parse(images);
-    images.splice(images.indexOf(middleCard.getAttribute('src')), 1);
-    for (let i = 0; i < playerCards.length; i++) {
-        if (i == randomIndex) {
-            playerCards[i].src = middleCard.getAttribute('src');
-        } else {
-            let newCard = getRandomLink(images);
-            playerCards[i].src = newCard;
-            images.splice(images.indexOf(newCard), 1);
+    function highlight(event) {
+        event.target.select();
+    }
+
+
+    function getPlayerCards(playerCards, middleCard) {
+        let randomIndex = Math.floor(Math.random() * playerCards.length);
+        let images = document.getElementById("file-container").dataset.files;
+        images = JSON.parse(images);
+        images.splice(images.indexOf(middleCard.getAttribute('src')), 1);
+        for (let i = 0; i < playerCards.length; i++) {
+            if (i == randomIndex) {
+                playerCards[i].src = middleCard.getAttribute('src');
+            } else {
+                let newCard = getRandomLink(images);
+                playerCards[i].src = newCard;
+                images.splice(images.indexOf(newCard), 1);
+            }
         }
     }
-}
 
 
-function getCards(){
-    let images = document.getElementById("file-container").dataset.files;
-    images = JSON.parse(images);
-    let middleCard = document.querySelector('.middle-pic');
-    middleCard.setAttribute('src', getRandomLink(images));
-    let player1Cards = document.querySelectorAll('.player-1-card');
-    let player2Cards = document.querySelectorAll('.player-2-card');
-    getPlayerCards(player1Cards, middleCard);
-    getPlayerCards(player2Cards, middleCard);
+    function getCards() {
+        let images = document.getElementById("file-container").dataset.files;
+        images = JSON.parse(images);
+        let middleCard = document.querySelector('.middle-card');
+        middleCard.setAttribute('src', getRandomLink(images));
+        let player1Cards = document.querySelectorAll('.player-1-card');
+        let player2Cards = document.querySelectorAll('.player-2-card');
+        getPlayerCards(player1Cards, middleCard);
+        getPlayerCards(player2Cards, middleCard);
 
 
-}
-
-
-function compareCards(card1, card2) {
-
-}
-
-function inputKeyUp(event) {
-    if (event.key == 'a') {
-        console.log('vüzdrszüd')
     }
+
+
+    function compareCards(event) {
+        let player_1_keys = document.getElementById('player_1').dataset.keys1;
+        player_1_keys = JSON.parse(player_1_keys);
+        let player_2_keys = document.getElementById('player_2').dataset.keys2;
+        player_2_keys = JSON.parse(player_2_keys);
+        let keyPressed = event.key.toUpperCase();
+        let middleCard = document.querySelector('.middle-card');
+
+        if (player_1_keys.includes(keyPressed)) {
+            let card = document.querySelector(`#${keyPressed}`);
+            if (middleCard.getAttribute('src') == card.getAttribute('src')) {
+                score1++;
+            } else {
+                score1--;
+            }
+            document.querySelector('#score1').innerHTML = `Score: ${score1}`;
+        }
+        if (player_2_keys.includes(keyPressed)) {
+            let card = document.querySelector(`#${keyPressed}`);
+            if (middleCard.getAttribute('src') == card.getAttribute('src')) {
+                score2++;
+            } else {
+                score2--;
+            }
+            document.querySelector('#score2').innerHTML = `Score: ${score2}`;
+        }
+    }
+
+
 }
 
 
