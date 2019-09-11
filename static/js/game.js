cd0972768f42b13b25f905b82db61c37c18338f0
Drosztmér István <drosztmer.istvan@gmail.com>
2019-09-11 10:54:55 +0200

    Fix displaying cards -> no doubles in columns

--- static/js/game.js
+++ static/js/game.js
@@ -3,6 +3,7 @@ function main() {
     try {
         document.getElementById('restart').addEventListener('click', retry);
     } catch (error) {
+
     }
 
     try {
@@ -11,41 +12,45 @@ function main() {
             player.addEventListener('click', highlight)
         }
     } catch (error) {
+
     }
 
     getCards()
 
 }
 
+
 function retry() {
     window.location.reload();
 }
+
 
 function getRandomLink(images) {
     return images[Math.floor(Math.random() * images.length)];
 }
 
+
 function highlight(event) {
     event.target.select();
 }
+
 
 function getPlayerCards(playerCards, middleCard) {
     let randomIndex = Math.floor(Math.random() * playerCards.length);
     let images = document.getElementById("file-container").dataset.files;
     images = JSON.parse(images);
+    images.splice(images.indexOf(middleCard.getAttribute('src')), 1);
     for (let i = 0; i < playerCards.length; i++) {
         if (i == randomIndex) {
             playerCards[i].src = middleCard.getAttribute('src');
-            images.splice(images.indexOf(middleCard.getAttribute('src')), 1);
-
         } else {
             let newCard = getRandomLink(images);
             playerCards[i].src = newCard;
             images.splice(images.indexOf(newCard), 1);
         }
-
     }
 }
+
 
 function getCards(){
     let images = document.getElementById("file-container").dataset.files;
