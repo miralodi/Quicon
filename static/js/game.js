04c3bf118f73c2277ab34ad8258fa73eca102978
Drosztmér István <drosztmer.istvan@gmail.com>
2019-09-11 10:46:53 +0200

    Display random cards

--- static/js/game.js
+++ static/js/game.js
@@ -13,21 +13,51 @@ function main() {
     } catch (error) {
     }
 
+    getCards()
+
 }
 
 function retry() {
     window.location.reload();
 }
 
-function getRandomLink() {
-    let images = document.getElementById("file-container").dataset.files;
-    images = JSON.parse(images);
-    let randomLink = images[Math.floor(Math.random() * images.length)];
-    return randomLink
+function getRandomLink(images) {
+    return images[Math.floor(Math.random() * images.length)];
 }
 
 function highlight(event) {
     event.target.select();
+}
+
+function getPlayerCards(playerCards, middleCard) {
+    let randomIndex = Math.floor(Math.random() * playerCards.length);
+    let images = document.getElementById("file-container").dataset.files;
+    images = JSON.parse(images);
+    for (let i = 0; i < playerCards.length; i++) {
+        if (i == randomIndex) {
+            playerCards[i].src = middleCard.getAttribute('src');
+            images.splice(images.indexOf(middleCard.getAttribute('src')), 1);
+
+        } else {
+            let newCard = getRandomLink(images);
+            playerCards[i].src = newCard;
+            images.splice(images.indexOf(newCard), 1);
+        }
+
+    }
+}
+
+function getCards(){
+    let images = document.getElementById("file-container").dataset.files;
+    images = JSON.parse(images);
+    let middleCard = document.querySelector('.middle-pic');
+    middleCard.setAttribute('src', getRandomLink(images));
+    let player1Cards = document.querySelectorAll('.player-1-card');
+    let player2Cards = document.querySelectorAll('.player-2-card');
+    getPlayerCards(player1Cards, middleCard);
+    getPlayerCards(player2Cards, middleCard);
+
+
 }
 
 main();
